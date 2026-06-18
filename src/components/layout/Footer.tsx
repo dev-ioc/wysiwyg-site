7b86a4c6d38316d47c9eca756d4e2b84fe762bb8
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-06-18 15:40:31 +0300

    modif footer page

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
   ];
 
   const navigationLinks = [
-    { label: "Accueil", href: "/#acueil" },
+    { label: "Accueil", href: "/#accueil" },
     { label: "À propos", href: "/about" },
     { label: "Actualités", href: "/actualites" },
     { label: "Services", href: "/#services" },
@@ -66,14 +66,25 @@ const Footer = () => {
     "Supply chain Management",
   ];
 
+  const recentArticles = [
+    "Test 1",
+    "Test 2",
+    "Test 3",
+    "Test 4",
+    "Test 5",
+    "Test 6",
+    "Test 7",
+  ];
+
   return (
     <footer
       role="contentinfo"
       aria-label="Pied de page"
       className="bg-[#F1F1F1]"
     >
-      <div className="w-full py-12 md:py-16 px-4 bg-[url('/images/bg-footer.png')] bg-cover bg-center bg-no-repeat min-h-[584px]">
-        <div className="container mx-auto">
+      <div className="relative w-full py-12 md:py-16 px-4 bg-[url('https://www.wysiwyg.fr/wp-content/uploads/elementor/thumbs/pexels-brett-sayles-4508751-r4vkxymyczvr9djw100tz1iyeskebu44sw56c319jo.jpg')] bg-cover bg-center bg-no-repeat min-h-[584px]">
+        <div className="absolute inset-0 bg-black/60"></div>
+        <div className="relative z-10 container mx-auto">
           <div className="flex flex-col md:flex-row items-center justify-between gap-6 md:gap-0">
             <div className="relative w-[150px] md:w-[188px] h-[120px] md:h-[148px]">
               <Image
@@ -100,53 +111,42 @@ const Footer = () => {
               ))}
             </div>
           </div>
-
-          <hr className="border-[#b3b4b5] w-full mt-6 md:mt-8" />
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12 py-8 md:py-12">
+          <hr className="border-white/20 w-full mt-6 md:mt-8" />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-6 py-8 md:py-12">
             <section
               aria-labelledby="contact-title"
               className="text-center md:text-left"
             >
               <h2
                 id="contact-title"
-                className="text-[20px] text-[#FFFFFF] font-poppins font-bold mb-4 md:mb-6"
+                className="text-[20px] text-white font-poppins font-bold mb-4 md:mb-6"
               >
                 Contactez-nous
               </h2>
-              <div className="text-black text-[15px] md:text-[16px] font-assistant leading-[1.8rem] space-y-3">
+              <div className="text-white/90 text-[14px] md:text-[15px] font-assistant leading-[1.8rem] space-y-3">
                 <p>
-                  WYSIWYG a été créée le 1er Avril 1999{" "}
+                  WYSIWYG a été créée le 1er Avril 1999
                   <br className="hidden md:block" />
                   par Xavier LOPEZ.
                 </p>
                 <address className="not-italic space-y-1">
                   <p>
-                    Téléphone :{" "}
-                    <a
-                      href="tel:+330892490071"
-                      className="hover:text-[#52BC71] transition-colors"
-                    >
-                      +33 08 92 49 00 71
-                    </a>
+                    <span className="text-[#52BC71]">Téléphone</span> : +33 08
+                    92 49 00 71
                   </p>
                   <p>
-                    E-mail :{" "}
-                    <a
-                      href="mailto:support@caspeo.fr"
-                      className="hover:text-[#52BC71] transition-colors"
-                    >
-                      support@caspeo.fr
-                    </a>
+                    <span className="text-[#52BC71]">E-mail</span> :
+                    support@caspeo.fr
                   </p>
                   <p>
-                    Site web :{" "}
+                    <span className="text-[#52BC71]">Site web </span> :{" "}
                     <a
-                      href="https://exemple.com"
+                      href="https://wysiwyg-website.onrender.com/"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="hover:text-[#52BC71] transition-colors"
                     >
-                      https://exemple.com
+                      https://wysiwyg-website.onrender.com
                     </a>
                   </p>
                 </address>
@@ -158,11 +158,11 @@ const Footer = () => {
             >
               <h2
                 id="services-title"
-                className="text-[20px] text-[#FFFFFF] font-poppins font-bold mb-4 md:mb-6"
+                className="text-[20px] text-white font-poppins font-bold mb-4 md:mb-6"
               >
                 Nos Services
               </h2>
-              <ul className="text-black text-[15px] md:text-[16px] font-assistant space-y-2 leading-[1.6rem]">
+              <ul className="text-white/90 text-[14px] md:text-[15px] font-assistant space-y-2 leading-[1.8rem]">
                 {servicesList.map((service) => (
                   <li key={service}>{service}</li>
                 ))}
@@ -175,7 +175,7 @@ const Footer = () => {
               <div className="flex items-center justify-between md:justify-start cursor-pointer md:cursor-default">
                 <h2
                   id="navigation-title"
-                  className="text-[20px] text-[#FFFFFF] font-poppins font-bold"
+                  className="text-[20px] text-white font-poppins font-bold"
                 >
                   Navigation
                 </h2>
@@ -195,7 +195,7 @@ const Footer = () => {
 
               <nav
                 aria-label="Navigation principale"
-                className={`flex flex-col gap-3 mt-4 md:mt-6 ${
+                className={`flex flex-col gap-2 mt-4 md:mt-6 ${
                   isNavOpen ? "block" : "hidden md:flex"
                 }`}
               >
@@ -203,7 +203,7 @@ const Footer = () => {
                   <Link
                     key={link.label}
                     href={link.href}
-                    className="text-black text-[15px] md:text-[16px] hover:text-white transition-colors duration-300 relative group w-fit mx-auto md:mx-0"
+                    className="text-white/90 text-[14px] md:text-[15px] hover:text-[#52BC71] transition-colors duration-300 relative group w-fit mx-auto md:mx-0"
                   >
                     {link.label}
                     <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-[#52BC71] transition-all duration-300 group-hover:w-full"></span>
@@ -211,17 +211,43 @@ const Footer = () => {
                 ))}
               </nav>
             </section>
+            <section
+              aria-labelledby="articles-title"
+              className="text-center md:text-left"
+            >
+              <h2
+                id="articles-title"
+                className="text-[20px] text-white font-poppins font-bold mb-4 md:mb-6"
+              >
+                Dernier article
+              </h2>
+              <ul className="text-white/90 text-[14px] md:text-[15px] font-assistant space-y-2 leading-[1.8rem]">
+                {recentArticles.map((article, index) => (
+                  <li key={index}>
+                    <Link
+                      href={`/blog/${article.toLowerCase().replace(/ /g, "-")}`}
+                      className="hover:text-[#52BC71] transition-colors duration-300"
+                    >
+                      {article}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </section>
           </div>
         </div>
       </div>
+
       <div className="bg-black w-full py-4 md:py-6">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 text-white gap-2 md:gap-0">
           <p className="text-sm md:text-base text-center">
             &copy; Copyright {currentYear} WYSIWYG. Tous droits réservés
           </p>
-          <p className="text-sm md:text-base text-center">
-            Conception par l&apos;équipe IOC
-          </p>
+          <div className="flex items-center gap-4">
+            <p className="text-sm md:text-base text-center">
+              Conception par l&apos;équipe IOC
+            </p>
+          </div>
         </div>
       </div>
     </footer>
