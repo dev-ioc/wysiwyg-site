42f6485414722ed576cd7f0c6c5067e130e3f639
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-06-24 10:37:10 +0300

    add all news, news page

--- src/components/sections/News.tsx
+++ src/components/sections/News.tsx
@@ -1,11 +1,13 @@
+import { BackpackIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const News = () => {
   const t = useTranslations("News");
 
   return (
     <section className="w-full bg-[#F1F1F1] dark:bg-slate-900 dark:text-white border-b border-gray-200 dark:border-slate-700">
-      <div className="container mx-auto px-6 lg:px-12 py-32 space-y-12">
+      <div className="container mx-auto px-6 lg:px-12 py-32 space-y-16">
         <div className="grid grid-cols-1 lg:grid-cols-3 items-center">
           <div />
 
@@ -25,7 +27,6 @@ const News = () => {
 
           <div />
         </div>
-
         <hr className="w-full border-grey dark:border-gray-600 border-t-2" />
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="flex flex-col gap-4">
@@ -126,8 +127,196 @@ const News = () => {
           <span className="whitespace-nowrap text-2xl lg:text-[32px] font-assistant font-bold ">
             {t("AllNews")}
           </span>
-
           <hr className="flex-1 border-t-2 border-grey dark:border-gray-600" />
+        </div>
+        <div className="grid grid-cols-1  gap-6">
+          <div className="flex lg:flex-row lg:gap-8 flex-col gap-4">
+            <div className="lg:w-[50%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo
+                  vel quis deleniti modi aliquid, voluptatum, doloremque
+                  voluptas,
+                </span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[25%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[25%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center  lg:h-[400px] h-[200px]" />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex lg:flex-row lg:gap-8 flex-col gap-4 w-full">
+            <div className="lg:w-[23%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[23%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[50%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-4 gap-4 grid-cols-1">
+            <div className="w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center h-[30vh] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center h-[30vh] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center h-[30vh] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center h-[30vh] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex lg:flex-row lg:gap-8 flex-col gap-4">
+            <div className="lg:w-[50%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo
+                  vel quis deleniti modi aliquid, voluptatum, doloremque
+                  voluptas,
+                </span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[25%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center lg:h-[400px] h-[200px] " />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-[25%] w-full">
+              <div className="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-center  lg:h-[400px] h-[200px]" />
+              <div className="flex flex-col gap-4 py-6">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <div className="flex gap-2 items-center text-gray-500 text-sm">
+                  <span>{new Date().toLocaleDateString()}</span>
+                  <span className="border-l border-gray-400 pl-2">By Roby</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid lg:grid-cols-3 grid-cols-1">
+            <div className="w-full flex flex-row gap-2 items-center md:text-center">
+              <span>1</span>
+              <span>2</span>
+              <span>3</span>
+              <span>...</span>
+              <span>10</span>
+              <FaArrowLeft />
+              <FaArrowRight />
+            </div>
+            <div className="w-full text-center">
+              <button className=" justify-center items-center w-[100px] h-[100px] rounded-full border border-red-400 text-red-400">
+                Voir Plus
+              </button>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+          <div className="flex flex-col gap-4">
+            <h1>Security</h1>
+            <label>AI</label>
+            <label>GADGETS</label>
+            <label>MILITARY</label>
+          </div>
+          <div>
+            <p>
+              Grâce à nos Solutions Sécurités Avancés, nous accompagnons les
+              Entreprises dans la protection des données, la sécurisation des
+              réseaux et des attaques extérieures aux infrastructures
+              informatiques. L’équipe de Wysiwyg est prête à vous aider à
+              appliquer les mesures de défense les plus pertinentes pour votre
+              environnement informatique. N’hésitez pas à nous contacter pour
+              une consultation gratuite sur n’importe quel problème de sécurité
+              que vous avez, et nous définirons et mettrons en œuvre une manière
+              optimale de l’aborder.
+            </p>
+          </div>
+          <div className="bg-[url('/images/pexels-fauxels-3183197-768x512.jpg.webp')] bg-no-repeat bg-cover bg-center h-[40vh] " />
+
+          <div className="bg-[url('/images/pexels-burst-374074-r4vkxxp465ugxrl96hm7ejrhtep1450egrhout2npw.jpg.webp')] bg-no-repeat bg-cover bg-center h-[40vh] " />
         </div>
       </div>
     </section>
