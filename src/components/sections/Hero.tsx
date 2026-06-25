1e6768c748ca06e5234760545118d625bdfafbd2
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-06-25 15:39:22 +0300

    modif hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,8 @@ import { useTranslations } from "next-intl";
 const Hero = () => {
   const t = useTranslations("Hero");
   return (
-    <div className="relative grid grid-cols-12 col-span-12 h-auto lg:h-[761px] w-full bg-[url('/images/pexels-christina-morillo-1181341-768x512.jpg.webp')] bg-cover bg-center bg-no-repeat overflow-hidden pb-10">
+    <div className="relative grid grid-cols-12 col-span-12 h-auto lg:h-[761px] w-full bg-[url('/images/hero.png')] bg-cover bg-center bg-no-repeat overflow-hidden pb-10 ">
+      <div className="absolute inset-0 bg-black/200 opacity-50 backdrop-blur-sm" />
       <div className="col-span-12 lg:col-start-5 flex flex-col items-center justify-center text-center w-full gap-6 relative z-10 pt-20 lg:pt-20 lg:-top-5 lg:w-[700px]">
         <h1 className="mb-2">
           <span className="relative inline-block font-mono">
@@ -32,10 +33,10 @@ const Hero = () => {
           </span>
         </h1>
 
-        <span className="max-w-[500px] text-[16px] lg:text-[20px] text-center text-secondary font-assistant font-bold lg:leading-[2rem]">
+        <span className="max-w-[500px] text-[16px] lg:text-[22px] text-center text-secondary font-assistant font-bold lg:leading-[2rem]">
           {t("title")}
         </span>
-        <button className="bg-secondary px-6 py-2 rounded-full text-black font-medium transition font-assistant">
+        <button className="bg-secondary px-6 py-2 rounded-full text-black font-medium transition font-assistant text-[21px]">
           {t("BtnPlus")}
         </button>
       </div>
