750edd1fe21071535385cd529fe623c275b4e692
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-07-13 16:07:40 +0300

    add logo favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "WYSIWYG",
   description: "What You See Is What You Get",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
