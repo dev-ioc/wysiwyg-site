ee32ac4109bc385a3afe5c2b29ed638c690f24fc
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-07-27 11:19:14 +0300

    routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,5 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   locales: ["en", "fr"],
-
-  // Used when no locale matches
   defaultLocale: "fr",
 });
