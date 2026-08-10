0a5d1efd10e65fc24d9fa4cef35ad3f5703b328b
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-08-10 13:35:40 +0300

    modif legal notice

--- src/components/legla-notice/LegalNotice.tsx
+++ src/components/legla-notice/LegalNotice.tsx
@@ -8,12 +8,11 @@ const LegalNotice = () => {
   ) as string[];
 
   return (
-    <div className="bg-white dark:bg-slate-900">
-      <div className="grid grid-cols-1 container mx-auto max-w-6xl pt-20 space-y-10 mb-20 px-6 py-6">
+    <div className="bg-white dark:bg-slate-900 pb-10">
+      <div className="grid grid-cols-1 container mx-auto max-w-6xl pt-20 space-y-10 px-6 py-6">
         <h1 className="text-xl md:text-6xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("title")}
         </h1>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("agreement.title")}
         </h2>
@@ -23,28 +22,24 @@ const LegalNotice = () => {
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           {t("agreement.paragraph2")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("modification.title")}
         </h2>
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           {t("modification.content")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("linksToThirdPartySites.title")}
         </h2>
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           {t("linksToThirdPartySites.content")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("noUnlawfulUse.title")}
         </h2>
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           {t("noUnlawfulUse.content")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("communicationServices.title")}
         </h2>
@@ -73,7 +68,6 @@ const LegalNotice = () => {
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           {t("communicationServices.paragraph4")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("materialsProvided.title")}
         </h2>
@@ -86,7 +80,6 @@ const LegalNotice = () => {
         <p className="text-lg md:text-[20px] text-black dark:text-white/50">
           {t("materialsProvided.paragraph3")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("liabilityDisclaimer.title")}
         </h2>
@@ -99,7 +92,6 @@ const LegalNotice = () => {
         <p className="text-lg md:text-[20px] text-black dark:text-white/50 uppercase">
           {t("liabilityDisclaimer.paragraph3")}
         </p>
-
         <p className="text-lg md:text-xl text-black dark:text-white/50">
           <strong>{t("serviceContact.title")}</strong> :{" "}
           <a
@@ -109,21 +101,18 @@ const LegalNotice = () => {
             {t("serviceContact.email")}
           </a>
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("terminationAccessRestriction.title")}
         </h2>
         <p className="text-lg md:text-[20px] text-black dark:text-white/50">
           {t("terminationAccessRestriction.content")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("copyrightTrademarkNotices.title")}
         </h2>
         <p className="text-lg md:text-[20px] text-black dark:text-white/50">
           {t("copyrightTrademarkNotices.content")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("trademarks.title")}
         </h2>
@@ -136,7 +125,6 @@ const LegalNotice = () => {
         <p className="text-lg md:text-[20px]text-black dark:text-white/50">
           {t("trademarks.paragraph3")}
         </p>
-
         <h2 className="text-xl md:text-3xl font-extrabold leading-tight mb-6 tracking-tight text-black dark:text-white/50">
           {t("noticesProcedureCopyright.title")}
         </h2>
