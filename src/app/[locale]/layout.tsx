72b7945ae4d1804ad329b7a7e09e9ba6d5bc99f3
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-08-11 16:55:52 +0300

    add openGraph

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -57,6 +57,14 @@ export async function generateMetadata({
       siteName: "WYSIWYG",
       title: seo.title,
       description: seo.description,
+      images: [
+        {
+          url: `${BASE_URL}/images/logo-footer.png`,
+          width: 1200,
+          height: 630,
+          alt: seo.title,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
